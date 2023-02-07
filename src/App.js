480dc4ad09e171119a824fa23cf6c74c0981c8f9
lissiemarie <elyssamunden@gmail.com>
2023-02-07 08:09:15 -0700

    added icon and styled with css

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <Study height={200}/>
-        
+        <Study height={200}/>
+        <h1>Dictionary</h1>
+        <h3>What word would you like to look up?</h3> 
       </header>
     </div>
   );
