d535798ef821ff3217ede0de44f0257b6ea13fe8
lissiemarie <elyssamunden@gmail.com>
2023-02-07 19:17:47 -0700

    added loop for all definitions

--- src/Meaning.js
+++ src/Meaning.js
@@ -7,9 +7,17 @@ export default function Meaning(props) {
     return (
         <div className="Meaning">
             <h4 className="partOfSpeech">{props.meaning.partOfSpeech}</h4>
-            <p>{props.meaning.definitions[0].definition}</p>
-            <p><em>{props.meaning.definitions[0].example}</em></p>
-            
+            {props.meaning.definitions.map(function(definition, index){
+                return(
+                    <div key={index}>
+                        <p>
+                        {definition.definition}
+                        <br />
+                        <em>{definition.example}</em>
+                        </p>
+                    </div>
+                )
+            })} 
         </div>
     )
 }
