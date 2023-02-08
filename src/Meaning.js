2be3f0784b9cf803a800340c5a795cd59397f86e
lissiemarie <elyssamunden@gmail.com>
2023-02-07 19:07:53 -0700

    fixed bug, added meaning and examples

--- src/Meaning.js
+++ src/Meaning.js
@@ -6,7 +6,10 @@ export default function Meaning(props) {
     console.log(props.meaning)
     return (
         <div className="Meaning">
-        "Hello"
+            <h4 className="partOfSpeech">{props.meaning.partOfSpeech}</h4>
+            <p>{props.meaning.definitions[0].definition}</p>
+            <p><em>{props.meaning.definitions[0].example}</em></p>
+            
         </div>
     )
 }
